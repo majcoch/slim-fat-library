90c655e3c5a9628bef9e11be0e1dd140b7d4f5ac
Michał Granda <majcoch98@gmail.com>
2020-09-03 13:50:03 +0200

    Create empty solution

---
+++ slim-fat-sln/slim-fat-runner/main.c
@@ -0,0 +1,18 @@
+/*
+ * slim-fat-runner.c
+ *
+ * Created: 03.09.2020 15:49:29
+ * Author : Micha³ Granda
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
