534c3a5487646b5704491ad3929f6b03a7f071f5
Michał Granda <majcoch98@gmail.com>
2020-09-06 19:22:03 +0200

    Implement SD card initialization

---
+++ src/sd-driver/commands.h
@@ -0,0 +1,34 @@
+/*
+ * commands.h
+ *
+ * Created: 05.09.2020 12:21:13
+ * Author : Micha³ Granda
+ */ 
+
+
+#ifndef COMMANDS_H_
+#define COMMANDS_H_
+
+
+#define GO_IDLE_STATE			0x40
+#define GO_IDLE_STATE_ARG		0x00000000
+#define GO_IDLE_STATE_CRC		0x95
+
+#define SEND_IF_COND			0x48
+#define SEND_IF_COND_ARG		0x000001AA
+#define SEND_IF_COND_CRC		0x87
+
+#define READ_OCR				0x7A
+#define READ_OCR_ARG			0x00000000
+#define READ_OCR_CRC			0x00
+
+#define APP_CMD					0x77
+#define APP_CMD_ARG				0x00000000
+#define APP_CMD_CRC				0x00
+
+#define SD_SEND_OP_COND			0x69
+#define SD_SEND_OP_COND_ARG		0x40000000	// SD HC accepted
+#define SD_SEND_OP_COND_CRC		0x00
+
+
+#endif /* COMMANDS_H_ */
