6efe5782f56b30a03926668c703514481ef2b37c
Michał Granda <majcoch98@gmail.com>
2020-10-13 18:21:47 +0200

    Implement FAT32 file system layer interface

---
+++ src/slimfat/fat32/fat32.h
@@ -0,0 +1,37 @@
+/*
+ * fat32.h
+ *
+ * Created: 06.10.2020 21:09:44
+ * Author : Micha³ Granda
+ */
+
+
+#ifndef FAT32_H
+#define FAT32_H
+
+#include <stdint.h>
+#include "../slimfaterr.h"
+#include "../storage/storage.h"
+
+typedef struct fs_fat32_partition {
+} fs_partition_t;
+
+typedef struct fat32_entry {
+} fat_entry_t;
+
+/* Partition operation */
+fs_error fat32_mount_partition(fs_partition_t* partition, const uint32_t start_sector);
+
+/* File entry operations */
+fs_error fat32_find_entry(const fs_partition_t* partition, fat_entry_t* file, const uint8_t* name, const uint8_t name_len);
+fs_error fat32_create_entry(const fs_partition_t* partition, fat_entry_t* entry, const uint8_t* name, const uint8_t name_len);
+fs_error fat32_update_entry(const fs_partition_t* partition, fat_entry_t* file);
+
+/* Cluster operations */
+uint32_t fat32_get_cluster_sector(const fs_partition_t* partition, const uint32_t* cluster);
+
+fs_error fat32_find_next_cluster(const fs_partition_t* partition, uint32_t* cluster);
+fs_error fat32_alloc_new_cluster(const fs_partition_t* partition, uint32_t* last_cluster);
+fs_error fat32_free_cluster_chain(const fs_partition_t* partition, uint32_t* first_cluster);
+
+#endif
